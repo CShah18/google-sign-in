406f501bee2425310a0942361299d9c3b7ab4583
CShah18 <chirageng.webforest@gmail.com>
2025-04-02 09:27:31 +0530

    first commit

--- passportConfig.js
+++ passportConfig.js
@@ -10,7 +10,7 @@ passport.use(
       callbackURL: "https://google-sign-in-1ydu.onrender.com/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("Google Profile:", profile);
+      // console.log("Google Profile:", profile);
       done(null, profile);
     }
   )
@@ -18,10 +18,12 @@ passport.use(
 
 // ✅ Store entire user profile in session
 passport.serializeUser((user, done) => {
+  console.log("Serialized User:", user);
   done(null, user);
 });
 
 // ✅ Retrieve full user from session
 passport.deserializeUser((user, done) => {
+  console.log("Deserialized User:", user);
   done(null, user);
 });
